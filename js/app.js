c78e1081109768c3573bef7a4105d0cd09ef772a
Nat Zimmermann <nat@natzim.com>
2014-09-01 19:49:51 +0100

    Added code to stop multiple request for the same user. Reformatted code

--- js/app.js
+++ js/app.js
@@ -1,30 +1,40 @@
+// Variables
+
+var usernames = ["", ""];
+
+// Functions
+
 var updatePanel = function(uid, val) {
   $("#user-" + uid + " > .panel").html(val);
 }
 
-var updateUserStats = function(obj) {
-  var user = obj.val(),
-    // RegExp to extract the number from the id
-    uid = obj.parent().attr("id").slice(-1);
+var updateUserStats = function(el) {
+  var user = el.val(),
+    uid = el.parent().attr("id").slice(-1),
+    aid = parseInt(uid, 10) - 1;
 
-  updatePanel(uid, "Loading data...");
-
-  $.ajax({
-    url: "http://www.reddit.com/user/" + user + "/about.json",
-    type: "GET",
-    success: function(ret) {
-      updatePanel(uid,
-        "<p>Link karma: " + ret.data.link_karma + "</p>" +
-        "<p>Comment karma: " + ret.data.comment_karma + "</p>"
-      );
-    },
-    error: function(jqXHR) {
-      if(jqXHR.status === 404) {
-        updatePanel(uid, "Whoops! User does not exist!");
+  if (usernames[aid] !== user) {
+    updatePanel(uid, "Loading data...");
+    $.ajax({
+      url: "http://www.reddit.com/user/" + user + "/about.json",
+      type: "GET",
+      success: function(ret) {
+        updatePanel(uid,
+          "<p>Link karma: " + ret.data.link_karma + "</p>" +
+          "<p>Comment karma: " + ret.data.comment_karma + "</p>"
+        );
+        usernames[aid] = user;
+      },
+      error: function(jqXHR) {
+        if(jqXHR.status === 404) {
+          updatePanel(uid, "Whoops! User does not exist!");
+        }
       }
-    }
-  });
+    });
+  } 
 }
+
+// Events
 
 $("#form-main").submit(function(e) {
   e.preventDefault();
@@ -35,8 +45,8 @@ $("#user-1 > input, #user-2 > input")
     updateUserStats($(this));
   })
   .keyup(function(e) {
+    // If the enter key is pressed while the focus is on one of the inputs
     if (e.keyCode === 13) {
       updateUserStats($(this));
     }
   });
-
