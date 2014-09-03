ba0631094c9d1bd17647bdc5cf8424a2bd344307
Nat Zimmermann <nat@natzim.com>
2014-09-03 15:38:17 +0100

    Added robots.txt and humans.txt

--- race/index.php
+++ race/index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/plain" rel="author" href="../humans.txt">
     <title>Reddit Karma Race</title>
     <link rel="stylesheet" href="../css/vendor/foundation.min.css">
     <script src="../js/vendor/modernizr.js"></script>
